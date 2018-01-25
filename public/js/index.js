d6bcacf31424262d623e16892c80d1a00b17a21c
joco25 <onungwej@gmail.com>
2018-01-25 23:54:34 +0100

    Added mustash.js

--- public/js/index.js
+++ public/js/index.js
@@ -18,21 +18,29 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message){
     let formatedTime = moment(message.createdAt).format('h:mm a')
-    let li = $('<li></li>');
-    li.text(`${message.from} ${formatedTime}: ${message.text}`);
-    $('#messages').append(li);
+
+    var template = $('#message-template').html();
+    var html = Mustache.render(template,{
+        text: message.text,
+        from: message.from,
+        createdAt: formatedTime
+    });
+
+    $('#messages').append(html);
+
 })
 
 socket.on('newLocationMessage',function(message){
     let formatedTime = moment(message.createdAt).format('h:mm a')
-    var li= $('<li></li>');
-    var a= $('<a target="_blank" >My current location</a>')
     
-    li.text(`${message.from} ${formatedTime}: `);
-    a.attr('href', message.url);
-
-    li.append(a);
-    $('#messages').append(li)
+    let template = $('#location-message-template').html();
+    var html = Mustache.render(template,{
+        url: message.url,
+        createdAt: formatedTime,
+        from: message.from
+    }) 
+    
+    $('#messages').append(html)
 })
 
 $('#message-form').on('submit',function (e){
