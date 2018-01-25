a1b59e6f6306dee34b27c07d298e8c592234aefc
joco25 <onungwej@gmail.com>
2018-01-25 21:27:45 +0100

    modified package.json to fix heroku start issue

--- server/server.js
+++ server/server.js
@@ -25,13 +25,11 @@ io.on('connection',(socket)=>{
         
         io.emit('newMessage',generateMessage(message.from, message.text));
         callback('this is from the server');
-
-        // socket.broadcast.emit('newMessage',{
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // })
     });
+
+    socket.on('createLocationMessage',(coords)=>{
+        io.emit('newMessage',generateMessage('Admin',`${coords.latitude}, ${coords.longitude}`))
+    })
 
     socket.on('disconnect', () => {
         console.log("client disconnected")
